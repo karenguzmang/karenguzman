b627453b90acf619524fa9b264599e926c90ba46
karen guzman <159082686+karenguzmang@users.noreply.github.com>
2024-02-08 16:10:44 -0700

    Update cis129_lab02_shortStory.py
    added a short story about my family

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+print(' On a clear sunny day, me and my family decided to go to the park and enjoy the beatiful day.
+At the park me and my family had a picnic and played soccer all day long. Once it got dark, we headed back home and had some
+ice cream we picked up on our way home. It was a wonderful day and I hope to do it again. ')
+# Author: Karen Guzman
+# My first python lab in my CIS129 course
+# Prints a simple short story
